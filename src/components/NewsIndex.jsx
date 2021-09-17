ebaa41d28acec25f18d7a483f7edd829931dec6c
maxarvid <max.arvid.anderson@gmail.com>
2021-09-17 16:03:58 +0200

    adds axios call to news API and stores it with React Hook in NewsIndex.jsx

--- src/components/NewsIndex.jsx
+++ src/components/NewsIndex.jsx
@@ -1,14 +1,28 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-import NewsCard from './NewsCard'
-
+import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import NewsCard from "./NewsCard";
+import axios from "axios";
 
 const NewsIndex = () => {
+  const [news, setNews] = useState([]);
+
+  const API_KEY = process.env.REACT_APP_API_KEY;
+  const API_URL = `https://newsapi.org/v2/everything?q=tesla&from=2021-08-17&sortBy=publishedAt&apiKey=${API_KEY}`;
+
+  const fetchData = async () => {
+    const response = await axios.get(API_URL);
+    setNews(response.data.articles);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <>
       <NewsCard />
     </>
-  )
-}
+  );
+};
 
-export default NewsIndex
+export default NewsIndex;
